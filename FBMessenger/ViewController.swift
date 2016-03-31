1cec5e8b402b1331d7847d6781d241e0a2b86ab9
Heron Yang <heron.yang.tw@gmail.com>
2016-03-31 15:27:05 +0800

    init

--- FBMessenger/ViewController.swift
+++ FBMessenger/ViewController.swift
@@ -7,13 +7,32 @@
 //
 
 import Cocoa
+import WebKit
 
-class ViewController: NSViewController {
+extension String {
+	subscript (i: Int) -> Character {
+		return self[self.startIndex.advancedBy(i)]
+	}
+}
 
+class ViewController: NSViewController, WebUIDelegate {
+
+	@IBOutlet weak var webView: WebView!
+	
+	private var windowTitle: String?;
+	private var latestNotification: String?;
+	
 	override func viewDidLoad() {
-		super.viewDidLoad()
+		super.viewDidLoad();
+		let url = NSURL(string: "https://messenger.com")
+		let request = NSURLRequest(URL: url!);
+		webView.preferences.setValue(true, forKey: "developerExtrasEnabled");
+		webView.applicationNameForUserAgent = "Safari/601.2.7";
+		webView.mainFrameURL = "https://messenger.com";
 
-		// Do any additional setup after loading the view.
+		webView.mainFrame.loadRequest(request);
+		
+		setupUpdateTitleTimer();
 	}
 
 	override var representedObject: AnyObject? {
@@ -21,7 +40,60 @@ class ViewController: NSViewController {
 		// Update the view, if already loaded.
 		}
 	}
+	
+	func setupUpdateTitleTimer() {
+		NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(ViewController.updateWindowTitle), userInfo: nil, repeats: true);
+	}
+	
+	func updateWindowTitle() {
+		if let windowTitle = webView.stringByEvaluatingJavaScriptFromString("document.title") {
+			handleDifferentTitle(windowTitle);
+		} else {
+			self.view.window!.title = "Messenger";
+		}
+	}
+	
+	func handleDifferentTitle(title: String) {
+		if title == "Messenger" || title == "" {
+			self.view.window!.title = "Heron's Messenger";
+		} else if (isTitleContainsUnreadCount(title)) {
+			self.view.window!.title = title;
+		} else {
+			self.view.window!.title = title;
+			showNotificationIfNew(title);
+		}
+	}
+	
+	func isTitleContainsUnreadCount(title: String)-> Bool {
+		if title[0] == "(" {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	func showNotificationIfNew(content: String) {
+		if latestNotification != content {
+			showNotification(content);
+			latestNotification = content;
+		}
+	}
 
+	func webView(sender: WebView!, runOpenPanelForFileButtonWithResultListener resultListener: WebOpenPanelResultListener!, allowMultipleFiles: Bool) {
+		let openDialog = NSOpenPanel()
+		if (openDialog.runModal() == NSFileHandlingPanelOKButton) {
+			let fileName: String = (openDialog.URL?.path)!
+			resultListener.chooseFilename(fileName);
+		}
+	}
+	
+	func showNotification(content: String) -> Void {
+		let notification = NSUserNotification();
+		notification.title = content;
+		notification.informativeText = "Heron's Messenger";
+		notification.soundName = NSUserNotificationDefaultSoundName;
+		NSUserNotificationCenter.defaultUserNotificationCenter().deliverNotification(notification);
+	}
 
 }
 
---
+++ README.md
@@ -0,0 +1,9 @@
+# Facebook Messenger Webview Wrapper
+
+This is simply a webview wrapper for Facebook messenger for Mac OS X, which means that you can have a single/standalone Facebook messenger on your Mac.
+
+[Download link](./FBMessenger.app)
+
+## Credit
+
+- Icons made by [Freepik](http://www.freepik.com) from [Flaticon](http://www.flaticon.com) is licensed by [Creative Commons BY 3.0](http://creativecommons.org/licenses/by/3.0/)
