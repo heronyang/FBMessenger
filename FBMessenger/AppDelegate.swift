2cd1332501562548318c2a90ae6796efa5ea451f
Heron Yang <heron.yang.tw@gmail.com>
2016-03-31 10:57:51 +0800

    Initial Commit

---
+++ FBMessenger/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FBMessenger
+//
+//  Created by heron on 3/31/16.
+//  Copyright © 2016 Heron Yang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ FBMessenger/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FBMessenger
+//
+//  Created by heron on 3/31/16.
+//  Copyright © 2016 Heron Yang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
